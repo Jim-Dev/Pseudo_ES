aeac5d7f21b503dcb20c547c4a9073d2e26ca8ba
Jimmy Toledo <jimmy.toledo.a@gmail.com>
2018-06-15 16:18:43 -0400

    Added automatic test using TestNG

--- Project/Compiler/src/info/jtoledo/compiler/Main.java
+++ Project/Compiler/src/info/jtoledo/compiler/Main.java
@@ -15,12 +15,19 @@ public class Main {
 	public static void main(String[] args) throws Exception 
 	{
 		ANTLRInputStream input = new ANTLRFileStream("code.demo");
+		System.out.println(compile(input));
+	
+	}
+	
+	public static String compile(ANTLRInputStream input)
+	{
 		DemoLexer lexer = new DemoLexer(input);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 		DemoParser parser = new DemoParser(tokens);
 		
 		ParseTree tree = parser.addition();
-		System.out.println(createJasminFile(new MyVisitor().visit(tree)));
+		
+		return createJasminFile(new MyVisitor().visit(tree));
 	}
 	
 	private static String createJasminFile(String instructions)
